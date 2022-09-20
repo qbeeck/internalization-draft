43d2a886a17974b329527cc3c441af41bbe3ef4c
qbeeck <64665123+qbeeck@users.noreply.github.com>
2022-09-20 17:46:36 +0300

    1 file modified

--- src/app/views/second-page/second-page.component.ts
+++ src/app/views/second-page/second-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-second-page',
